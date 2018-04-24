8ab88d4666faec190859ca62fe00e1dcc7b07edc
Brancen Gregory <brancengregory@gmail.com>
2018-04-24 18:45:00 -0500

    Solvedgit status, Needs cleanup

--- Problem 18/problem18.R
+++ Problem 18/problem18.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 
-data <- read.delim("~/Documents/Project Euler/Problem 18/data", header=FALSE, stringsAsFactors=FALSE)
-#View(data)
+data <- read.delim("~/Documents/project-euler/Problem 18/data", header=FALSE, stringsAsFactors=FALSE)
+View(data)
 
 likitysplit <- function (x) {
   stringr::str_split(x, " ")
@@ -19,4 +19,15 @@ for (i in 1:nrow(mat)) {
 }
 rm("i", "j", "num", "string")
 
-(nrow(mat)-1):1
+optimalPath <- function (mat) {
+  for (i in (nrow(mat)-1):1) {
+    for (j in 1:i) {
+      if ( mat[i+1, j] >= mat[i+1, j+1] ) {
+        mat[i,j] <- mat[i,j] + mat[i+1, j]  
+      } else {
+        mat[i,j] <- mat[i,j] + mat[i+1, j+1]
+      }
+    }
+  }
+  return(mat[1,1])
+}
