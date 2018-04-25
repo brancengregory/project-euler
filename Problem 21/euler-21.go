7c2f0c09cc9367d1c10ffd9688d66ace3808f04a
Brancen Gregory <brancengregory@gmail.com>
2018-04-25 14:02:54 -0500

    Minor syntax changes

--- Problem 21/euler-21.go
+++ Problem 21/euler-21.go
@@ -1,15 +1,14 @@
 package main
 
-import(
+import (
 	"fmt"
 )
 
-
 func getDivisors(x int) []int {
 	var vec []int
-	for i:=1; i<x; i++{
-		if x % i == 0 {
-			vec = append(vec,i)
+	for i := 1; i < x; i++ {
+		if x%i == 0 {
+			vec = append(vec, i)
 		}
 	}
 	return vec
@@ -17,14 +16,14 @@ func getDivisors(x int) []int {
 
 func sumDivisors(x []int) int {
 	var sum int
-	for i,_ := range x {
+	for i, _ := range x {
 		sum = sum + x[i]
 	}
 	return sum
 }
 
 func isAbundant(x int) bool {
-	if x>=12{	
+	if x >= 12 {
 		a := getDivisors(x)
 		b := sumDivisors(a)
 		if b > x {
@@ -34,21 +33,18 @@ func isAbundant(x int) bool {
 	return false
 }
 
-
 func main() {
-	
+
 	var abundvec []int
-	for i:=1; i<28123; i++ {
-		if isAbundant(i)==true {
-			abundvec = append(abundvec,i)
+	for i := 1; i < 28123; i++ {
+		if isAbundant(i) == true {
+			abundvec = append(abundvec, i)
 		}
 	}
-	
-	
-	
+
 	test := getDivisors(20)
 	test2 := sumDivisors(test)
 	test3 := isAbundant(20)
-	
-	fmt.Println(test,test2,test3,abundvec)
+
+	fmt.Println(test, test2, test3, abundvec)
 }
