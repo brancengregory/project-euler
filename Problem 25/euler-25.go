57c99f993e6c63101ba838e7dd1c6fa80376d599
Brancen Gregory <brancengregory@gmail.com>
2018-04-27 22:58:44 -0500

    Solved 25, needs cleanup

--- Problem 25/euler-25.go
+++ Problem 25/euler-25.go
@@ -1,23 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"math/big"
 )
 
-func ithFibBig(x int) (ans *big.Int) {
-	if x <= 0 {
-		fmt.Println("Val must be of positive length... Insert a larger value.")
-		return nil
-	} else if x <= 2 {
-		ans = big.NewInt(1)
-	} else {
-		ans = big.NewInt(0)
-		ans.Add(ithFibBig(x-1), ithFibBig(x-2))
-	}
-	return ans
-}
-
 func main() {
-	fmt.Println(ithFibBig(50))
+	a, b, c, counter := big.NewInt(int64(1)), big.NewInt(int64(2)), big.NewInt(int64(0)), 2
+	for len(a.String()) < 1000 {
+		c.Neg(a) //c is a temp variable
+		a.Add(a, b)
+		b.Neg(c)
+		counter++
+	}
+	println(counter)
 }
