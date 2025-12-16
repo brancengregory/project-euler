5f3b0660e95e51bb25bc19d5252446797ba7265c
Brancen Gregory <brancengregory@gmail.com>
2025-12-16 14:16:11 -0600

    perf: Improve problem 19 strategy

--- rust/euler_cli/src/problems/p0019.rs
+++ rust/euler_cli/src/problems/p0019.rs
@@ -2,27 +2,26 @@ use anyhow::Result;
 
 pub fn solve() -> Result<String> {
     let mut n_first_sundays = 0;
-    let month_days = vec![31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    let month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-    let mut day_of_week = 0;
+    let mut day_of_week = 2;
 
     for year in 1901..=2000 {
         let is_leap_year = year % 4 == 0;
 
-        month_days.iter().enumerate().for_each(|(month, days)| {
-            let n_days = if is_leap_year && month == 1 {
-                days + 1
-            } else {
-                *days
+        for (month_idx, &days) in month_days.iter().enumerate() {
+            if day_of_week == 0 {
+                n_first_sundays += 1
             };
 
-            for day in 0..n_days {
-                day_of_week = (day_of_week + 1) % 7;
-                if day == 0 && day_of_week == 6 {
-                    n_first_sundays += 1;
-                }
-            }
-        });
+            let n_days = if month_idx == 1 && is_leap_year {
+                29
+            } else {
+                days
+            };
+
+            day_of_week = (day_of_week + n_days) % 7;
+        }
     }
 
     Ok(n_first_sundays.to_string())
