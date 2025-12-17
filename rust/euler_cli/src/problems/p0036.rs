50e829ae4839bc0a59941d656f98ca41a4e1d7bf
Brancen Gregory <brancengregory@gmail.com>
2025-12-16 18:07:57 -0600

    perf: Optimize problem 36 to check equality of iterators instead of their collected values

--- rust/euler_cli/src/problems/p0036.rs
+++ rust/euler_cli/src/problems/p0036.rs
@@ -2,20 +2,14 @@ use anyhow::Result;
 use euler::is_palindrome;
 
 pub fn solve() -> Result<String> {
-    let ans: u64 = (1..1_000_000).filter_map(|i| {
-        if is_palindrome(i) {
+    let ans: u64 = (1..1_000_000)
+        .filter(|&i| is_palindrome(i))
+        .filter(|&i| {
             let num_bin = format!("{:b}", i);
-            let rev_bin: String = num_bin.chars().rev().collect();
 
-            if rev_bin == num_bin {
-                Some(i)
-            } else {
-                None
-            }
-        } else {
-            None
-        }
-    }).sum();
+            num_bin.chars().eq(num_bin.chars().rev())
+        })
+        .sum();
     
     Ok(ans.to_string())
 }
